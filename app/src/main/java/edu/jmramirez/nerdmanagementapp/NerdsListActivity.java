8ad92ec046f46b8dfe58fede04a5d450055c8d40
Jose Manuel Ramírez Martínez <savy@MacBook-Air.local>
2015-10-26 21:57:03 +0100

    first commit

---
+++ app/src/main/java/edu/jmramirez/nerdmanagementapp/NerdsListActivity.java
@@ -0,0 +1,13 @@
+package edu.jmramirez.nerdmanagementapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NerdsListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nerds_list);
+    }
+}
